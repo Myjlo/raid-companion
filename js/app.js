0af48cb05d10f75c313937e4586bc5ba3b6cc536
Myjlo <myjlo@Franks-MacBook-Pro.local>
2018-02-28 21:58:20 +0100

    Init

--- js/app.js
+++ js/app.js
@@ -1,54 +1,75 @@
-//=========================//
-//       INITIALIZE        //
-//=========================//
+
+
 
 $(document).ready(function(){
 
 //=========================//
-//         PAGES           //
+//   TOOLTIP DATA ARRAYS   //
 //=========================//
-  $( "#Home" ).load( "pages/Home.html");
-  $( "#Sandbox" ).load( "pages/Sandbox.html");
-  $( "#SpiritVale" ).load( "pages/Spirit_Vale.html");
-  $( "#SalvationPass" ).load( "pages/Salvation_Pass.html");
-  $( "#StrongholdOfTheFaithful" ).load( "pages/Stronghold_Of_The_Faithful.html");
-  $( "#BastionOfThePenitent" ).load( "pages/Bastion_Of_The_Penitent.html");
-  $( "#HallOfChains" ).load( "pages/Hall_Of_Chains.html");
-//=========================//
-//        TOOLTIPS         //
-//=========================//
-//Arrays for Data Check
   var boons = ['Aegis','Alacrity','Fury','Might','Protection','Quickness','Regeneration','Resistance','Retaliation','Stability','Swiftness','Vigor'];
   var effects = ['Barrier','Daze','Float','Knockback','Knockdown','Launch','Pull','Sink','Stun','Nourishment','Enhancement','Invulnerability','Revealed','Stealth','Superspeed'];
   var conditions = ['Bleeding','Blind','Burning','Chilled','Confusion','Crippled','Fear','Immobile','Poison','Slow','Taunt','Torment','Vulnerability','Weakness'];
   var buffs = ['Banner_Of_Strength'];
-//Trigger for each Tooltip input
-  $('.effect-Tooltip').each(function() {
-//Get Tooltip Data
-    var name = $(this).attr('data-name');
-    var type = $(this).attr('data-type');
-//Check Tooltip Data with Arrays
+  
+  var professions = ['Warrior','Berserker','Spellbreaker','Guardian','Dragonhunter','Firebrand','Revenant','Herald','Renegade','Ranger','Druid','Engineer','Scrapper','Holosmith','Thief','Daredevil','Deadeye','Elementalist','Tempest','Weaver','Mesmer','Chronomancer','Mirage','Necromancer','Reaper','Scourge'];
+
+//=========================//
+//     EFFECT TOOLTIPS     //
+//=========================//
+  $('.tooltip-effect').each(function() {
+    var name = $(this).attr('data-name').replace(/\b\w/g, l => l.toUpperCase());
+    var type = $(this).attr('data-type').replace(/\b\w/g, l => l.toUpperCase());
+
     if ((type == 'Boon' && $.inArray(name, boons) >-1) ||
         (type == 'Effect' && $.inArray(name, effects) >-1) ||
         (type == 'Condition' && $.inArray(name, conditions) >-1) ||
         (type == 'Buff' && $.inArray(name, buffs) >-1)) {
-//Return Tooltip if Data is valid
-      $(this).html('<img class="icon-Effect" src="./assets/Tooltips/' + type + 's/' + name + '.png"></img>');
-      $(this).addClass('tooltip');
-      $(this).attr('data-tooltip', '<img class="icon-Effect" src="./assets/Tooltips/' + type + 's/' + name + '.png"></img><span class="text-' + type + '"> ' + name.replace(/_/g, ' ') + '</span>');
-    }
-//Return Invalid Data Error
-    else {
-      $(this).html('<img class="icon-Effect" src="./assets/Tooltips/Invalid.png"></img>');
-      $(this).addClass('tooltip');
-      $(this).attr('data-tooltip', '<span class="text-Error">ERR: Invalid  Data ("' + type + ' : ' + name + '")</span>');  
+      $(this).html('<img class="image-effect" src="./assets/Tooltips/' + type + 's/' + name + '.png"></img>');
+      $(this).popup({
+          html      : '<img class="image-effect" src="./assets/Tooltips/' + type + 's/' + name + '.png"></img><span class="text-' + type + '"> ' + name.replace(/_/g, ' ') + '</span>',
+          variation : 'inverted basic',
+          position  : 'top center',
+          transition: 'fade up'
+        });
+    } else {
+      $(this).html('<img class="image-effect" src="./assets/Tooltips/Invalid.png"></img>');
+      $(this).popup({
+          html      : '<img class="image-effect" src="./assets/Tooltips/Invalid.png"></img><span class="text-Error">ERR: Invalid  Data</span>',
+          variation : 'inverted basic',
+          position  : 'top center',
+          transition: 'fade up'
+        });     
     };  
   });
 //=========================//
-//       INITIALIZE        //
+//   PROFESSION TOOLTIPS   //
 //=========================//
-  $('.tooltip').tooltip({position: 'top'});
-  $('.tabs').tabs();
-  $('.sidenav').sidenav();
-  $('.dropdown-trigger').dropdown({constrainWidth: false, coverTrigger: false});
+  $('.tooltip-profession').each(function() {
+    var profession = $(this).attr('data-profession').replace(/\b\w/g, l => l.toUpperCase());
+    var role = $(this).attr('data-role').replace(/\b\w/g, l => l.toUpperCase());
+
+    console.log(role)
+    if ($.inArray(profession, professions) >-1) {
+      $(this).html('<img class="image-effect" src="./assets/Tooltips/Professions/' + profession + '.png"></img>');
+      $(this).popup({
+          html      : '<img class="image-effect" src="./assets/Tooltips/Professions/' + profession + '.png"></img><span class="text-' + profession + '"> ' + profession + '</span><sub> (' + role + ') </sub>',
+          variation : 'inverted basic',
+          position  : 'top center',
+          transition: 'fade up'
+        });
+      if ($(this).attr('data-include-name') == 'true'){
+        $(this).append('<a href="https://wiki.guildwars2.com/wiki/' + profession + '" class="text-' + profession + '">' + profession + '</a>')
+      }
+
+    } else {
+      $(this).html('<img class="image-effect" src="./assets/Tooltips/Invalid.png"></img>');
+      $(this).popup({
+          html      : '<img class="image-effect" src="./assets/Tooltips/Invalid.png"></img><span class="text-Error">ERR: Invalid  Data</span>',
+          variation : 'inverted basic',
+          position  : 'top center',
+          transition: 'fade up'
+        });     
+    }; 
+
+});
 });
